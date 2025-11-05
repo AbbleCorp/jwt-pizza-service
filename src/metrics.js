466d756d9a702e9ce194f31181e4e01d551e3d6f
AbbleCorp <a.gitgud@theomers.com>
2025-11-04 18:36:38 -0700

    add metrics.js file, need to write functions to increment or calculate

---
+++ src/metrics.js
@@ -0,0 +1,122 @@
+const config = require('./config');
+
+// Metrics stored in memory
+let requests = {};
+let cpuPercentage = 0;
+let memoryPercentage = 0;
+let successfulAuthentications = 0;
+let failedAuthentications = 0;
+let pizzasSold = 0;
+let revenueGenerated = 0;
+let creationLatencyMs = 0;
+let creationFailures = 0;
+
+
+
+const os = require('os');
+
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+
+function requestTracker(req, res, next) {
+  const endpoint = `[${req.method}] ${req.path}`;
+  requests[endpoint] = (requests[endpoint] || 0) + 1;
+  next();
+}
+
+// This will periodically send metrics to Grafana
+setInterval(() => {
+  const metrics = [];
+  Object.keys(requests).forEach((endpoint) => {
+    metrics.push(createMetric('requests', requests[endpoint], '1', 'sum', 'asInt', { endpoint }));
+  });
+
+  cpuPercentage = getCpuUsagePercentage();
+  metrics.push(createMetric('cpuPercentage', cpuPercentage, 'percent', 'sum', 'asDouble', {}));
+
+  memoryPercentage = getMemoryUsagePercentage();
+  metrics.push(createMetric('memoryPercentage', memoryPercentage, 'percent', 'sum', 'asDouble', {}));
+
+  metrics.push(createMetric('successfulAuthentications', successfulAuthentications, '1', 'sum', 'asInt', {}));
+  metrics.push(createMetric('failedAuthentications', failedAuthentications, '1', 'sum', 'asInt', {}));
+
+  metrics.push(createMetric('pizzasSold', pizzasSold, '1', 'sum', 'asInt', {}));
+  metrics.push(createMetric('revenueGenerated', revenueGenerated, 'USD', 'sum', 'asDouble', {}));
+
+  metrics.push(createMetric('creationLatencyMs', creationLatencyMs, 'ms', 'sum', 'asDouble', {}));
+  metrics.push(createMetric('creationFailures', creationFailures, '1', 'sum', 'asInt', {}));
+
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+  attributes = { ...attributes, source: config.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === 'sum') {
+    metric[metricType].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+
+  fetch(`${config.url}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { Authorization: `Bearer ${config.apiKey}`, 'Content-Type': 'application/json' },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error('Error pushing metrics:', error);
+    });
+}
+
+module.exports = { requestTracker };
